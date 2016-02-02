b30eb3f0800f4d68a685d634fe78dfdfbe686448
simnic31 <niki.sama@gmail.com>
2016-02-01 17:03:45 -0800

    ASST0 proj1

--- os161/kern/test/threadfun.c
+++ os161/kern/test/threadfun.c
@@ -36,15 +36,45 @@ init_sem(void)
 static int counter;
 
 
+int threadfun(int type)
+{	
+	(void)type;
+	init_sem();
+	counter = 0;
+	char name[16];
+	int i, result;
+
+	for (i=0; i<12; i++) {
+		snprintf(name, sizeof(name), "threadtest%d", i);
+		result = thread_fork(name, NULL,
+				     threadUse2, NULL, i);
+		if (result) {
+			panic("threadtest: thread_fork failed %s)\n", 
+			      strerror(result));
+		}
+	}
+
+
+	for (i=0; i<12; i++) {
+		P(tsem);
+	}
+	
+	kprintf("%d", counter);
+	counter = counter -119999;
+	counter = counter + '0';
+
+	return 0;
+}
+
 void
 threadUse(void *junk, unsigned long num)
 {
 	int ch = '0' + num;
 	for(int i; i < 10000; i++){
 	counter++;
-	}
 	(void)junk;
 	(void) ch;
+	}
 	V(tsem);
 
 }
@@ -57,7 +87,7 @@ threadUse2(void *junk, unsigned long num)
 	//(void)ch;
 	//(void)junk;
 
-	for (int i; i<1; i++){
+	for (int i; i<10000; i++){
 		P(tsem2);
 		counter++;
 		(void)junk;
@@ -67,38 +97,6 @@ threadUse2(void *junk, unsigned long num)
 	V(tsem);
 }
 
-
-int threadfun(int type)
-{	(void)type;
-	init_sem();
-	counter = 0;
-	char name[16];
-	int i, result;
-
-	for (i=0; i<NTHREADS; i++) {
-		snprintf(name, sizeof(name), "threadtest%d", i);
-		result = thread_fork(name, NULL,
-				     threadUse2,
-				     NULL, i);
-		if (result) {
-			panic("threadtest: thread_fork failed %s)\n", 
-			      strerror(result));
-		}
-	}
-
-
-	for (i=0; i<NTHREADS; i++) {
-		P(tsem);
-	}
-	
-	kprintf("%d", counter);
-	counter = counter -119999;
-	counter = counter + '0';
-	
-	//putch(counter);
-
-	return 0;
-}
 
 int test1(int nargs, char **args)
 {
@@ -138,3 +136,4 @@ int test3(int nargs, char **args)
 
 	return 0;
 }
+
